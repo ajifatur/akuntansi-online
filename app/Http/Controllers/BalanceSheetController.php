2c3e3a89e52cbbde0253a641d15c36c9d7fee486
ajifatur <ajifatur2@gmail.com>
2021-06-07 14:34:17 +0700

    Add balancesheet multiperiod, fix bugs

--- app/Http/Controllers/BalanceSheetController.php
+++ app/Http/Controllers/BalanceSheetController.php
@@ -10,6 +10,9 @@ use App\Kantor;
 
 class BalanceSheetController extends Controller
 {
+	// 13: CDM
+	// 1673: PT. Uji Coba
+
     /**
      * Menampilkan neraca standard
      * 
@@ -22,9 +25,7 @@ class BalanceSheetController extends Controller
 		$tanggal = $request->query('tanggal') != null ? generate_date_format($request->query('tanggal'), 'y-m-d') : date('Y-m-d');
 		
 		// Data user
-		// 13: CDM
-		// 1673: PT. Uji Coba
-		$user = DB::table('tbl_users')->where('id','=',13)->first();
+		$user = DB::table('tbl_users')->where('id','=',1673)->first();
 
 		if($user){
 			// Data kantor
@@ -33,6 +34,38 @@ class BalanceSheetController extends Controller
 			// View
 			return view('admin.balancesheet.standard', [
 				'tanggal' => $tanggal,
+				'user' => $user,
+				'kantor' => $kantor,
+			]);
+		}
+    }
+
+    /**
+     * Menampilkan neraca multiperiod
+     * 
+     * @return \Illuminate\Http\Request
+     * @return \Illuminate\Http\Response
+     */
+    public function multiperiod(Request $request)
+    {
+		// Bulan dan tahun
+		$bulan1 = date('n') - 3;
+		$bulan1 = $bulan1 < 1 ? 1 : $bulan1;
+		$bulan2 = date('n');
+		$tahun = date('Y');
+		
+		// Data user
+		$user = DB::table('tbl_users')->where('id','=',1673)->first();
+
+		if($user){
+			// Data kantor
+			$kantor = Kantor::where('groupid','=',$user->groupid)->get();
+
+			// View
+			return view('admin.balancesheet.multiperiod', [
+				'bulan1' => $bulan1,
+				'bulan2' => $bulan2,
+				'tahun' => $tahun,
 				'user' => $user,
 				'kantor' => $kantor,
 			]);
@@ -51,10 +84,9 @@ class BalanceSheetController extends Controller
 		ini_set('memory_limit','1000M');
 
 		// Variabel
+		$grup = $request->query('grup');
 		$tanggal = $request->query('tanggal') != null ? generate_date_format($request->query('tanggal'), 'y-m-d') : date('Y-m-d');
 		$kantor = $request->query('kantor');
-		$grup = $request->query('grup');
-		$html = '';
 		$dtotal = 0; // Total detail
 		$subtotal = 0; // Total harta, kewajiban, modal
 		$pasiva = 0; // Total pasiva
@@ -64,7 +96,7 @@ class BalanceSheetController extends Controller
 		$earning = get_earning($tanggal, $kantor, $grup);
 		
 		// Data neraca
-		$neraca = DB::select('SELECT a.*, (SELECT grup FROM ac_akuntipe WHERE idtipe=a.akuntipe) AS grup, (SELECT COUNT(kodeakun) FROM ac_akun WHERE idtipe=a.akuntipe) AS cnt FROM ac_fstmtlin a WHERE tipe=1 ORDER BY seq ASC');
+		$neraca = DB::select("SELECT a.*, (SELECT grup FROM ac_akuntipe WHERE idtipe=a.akuntipe) AS grup, (SELECT COUNT(kodeakun) FROM ac_akun WHERE idtipe=a.akuntipe) AS cnt FROM ac_fstmtlin a WHERE tipe=1 ORDER BY seq ASC");
 		
 		// Loop neraca
 		foreach($neraca as $data){
@@ -88,18 +120,18 @@ class BalanceSheetController extends Controller
 						if($data2->parent > 0){
 							$parentamount = $balance;
 							$parent = true;
-							$html .= '<tr>';
-							$html .= '<td style="text-indent: 2rem;">'.$data2->namaakun.'</td>';
-							$html .= '<td></td>';
-							$html .= '<td></td>';
-							$html .= '</tr>';
+							echo '<tr>';
+							echo '<td style="text-indent: 2rem;">'.$data2->namaakun.'</td>';
+							echo '<td></td>';
+							echo '<td></td>';
+							echo '</tr>';
 						}
 						else{
-							$html .= '<tr>';
-							$html .= '<td style="text-indent: 2rem;">'.$data2->namaakun.'</td>';
-							$html .= '<td align="right">'.$sbalance.'</td>';
-							$html .= '<td></td>';
-							$html .= '</tr>';
+							echo '<tr>';
+							echo '<td style="text-indent: 2rem;">'.$data2->namaakun.'</td>';
+							echo '<td align="right">'.$sbalance.'</td>';
+							echo '<td></td>';
+							echo '</tr>';
 						}
 
 						if($parent == true){
@@ -117,11 +149,11 @@ class BalanceSheetController extends Controller
 									$pasiva += $balance;
 									$sbalance = $balance >= 0 ? number_format($balance,0,',',',') : '('.number_format(abs($balance),0,',',',').')'; // String balance
 									$sdtotal = $dtotal >= 0 ? number_format($dtotal,0,',',',') : '('.number_format(abs($dtotal),0,',',',').')'; // String total detail
-									$html .= '<tr>';
-									$html .= '<td style="text-indent: 3rem;">'.$row->namaakun.'</td>';
-									$html .= '<td align="right">'.$sbalance.'</td>';
-									$html .= '<td></td>';
-									$html .= '</tr>';
+									echo '<tr>';
+									echo '<td style="text-indent: 3rem;">'.$row->namaakun.'</td>';
+									echo '<td align="right">'.$sbalance.'</td>';
+									echo '<td></td>';
+									echo '</tr>';
 								}
 							}
 						}
@@ -133,11 +165,11 @@ class BalanceSheetController extends Controller
 				if($data->stmtlinid != 50){
 					// $earning = get_earning($tanggal, $kantor, $grup);
 					$searning = $earning >= 0 ? number_format($earning,0,',',',') : '('.number_format(abs($earning),0,',',',').')'; // String earning
-					$html .= '<tr>';
-					$html .= '<td style="text-indent: 2rem;">'.$data->lineheading.' '.$data->idtipe.'</td>';
-					$html .= '<td align="right">'.$searning.'</td>';
-					$html .= '<td></td>';
-					$html .= '</tr>';
+					echo '<tr>';
+					echo '<td style="text-indent: 2rem;">'.$data->lineheading.' '.$data->idtipe.'</td>';
+					echo '<td align="right">'.$searning.'</td>';
+					echo '<td></td>';
+					echo '</tr>';
 					$dtotal += $earning; // Total detail
 					$subtotal += $earning; // Total parent
 					$pasiva += $earning;
@@ -148,48 +180,48 @@ class BalanceSheetController extends Controller
 				if($data->akuntipe > 0){
 					// Akun
 					if($data->linetype == 'Total'){
-						$html .= '<tr class="font-weight-bold">';
-						$html .= '<td style="text-indent: 1rem;">'.$data->lineheading.'</td>';
-						$html .= '<td></td>';
-						$html .= '<td align="right">'.$sdtotal.'</td>';
-						$html .= '</tr>';
+						echo '<tr class="font-weight-bold">';
+						echo '<td style="text-indent: 1rem;">'.$data->lineheading.'</td>';
+						echo '<td></td>';
+						echo '<td align="right">'.$sdtotal.'</td>';
+						echo '</tr>';
 					}
 					// Yang punya child saja
 					elseif($data->cnt > 0){
-						$html .= '<tr class="font-weight-bold">';
-						$html .= '<td colspan="3" style="text-indent: 1rem;">'.$data->lineheading.'</td>';
-						$html .= '</tr>';
+						echo '<tr class="font-weight-bold">';
+						echo '<td colspan="3" style="text-indent: 1rem;">'.$data->lineheading.'</td>';
+						echo '</tr>';
 					}
 				}
 				else{
 					$ssubtotal = $subtotal >= 0 ? number_format($subtotal,0,',',',') : '('.number_format(abs($subtotal),0,',',',').')'; // String sub total
 					$spasiva = $pasiva >= 0 ? number_format($pasiva,0,',',',') : '('.number_format(abs($pasiva),0,',',',').')'; // String pasiva
 					if($data->linetype == 'Heading'){
-						$html .= '<tr class="font-weight-bold" style="background-color: #efefef;">';
-						$html .= '<td colspan="3">'.$data->lineheading.'</td>';
-						$html .= '</tr>';
+						echo '<tr class="font-weight-bold" style="background-color: #efefef;">';
+						echo '<td colspan="3">'.$data->lineheading.'</td>';
+						echo '</tr>';
 					}
 					else{
 						// Total kewajiban dan modal
 						if($data->seq == 53){
-							$html .='<tr class="font-weight-bold" style="background-color: #ddd;">';
-							$html .='<td colspan="2" align="center">'.$data->lineheading.'</td>';
-							$html .='<td align="right">'.$spasiva.'</td>';
-							$html .='</tr>';
+							echo'<tr class="font-weight-bold" style="background-color: #ddd;">';
+							echo'<td colspan="2" align="center">'.$data->lineheading.'</td>';
+							echo'<td align="right">'.$spasiva.'</td>';
+							echo'</tr>';
 						}
 						// Total harta
 						elseif($data->seq == 29){
-							$html .= '<tr class="font-weight-bold" style="background-color: #ddd;">';
-							$html .= '<td colspan="2" align="center">'.$data->lineheading.'</td>';
-							$html .= '<td align="right">'.$ssubtotal.'</td>';
-							$html .= '</tr>';
+							echo '<tr class="font-weight-bold" style="background-color: #ddd;">';
+							echo '<td colspan="2" align="center">'.$data->lineheading.'</td>';
+							echo '<td align="right">'.$ssubtotal.'</td>';
+							echo '</tr>';
 							$subtotal = 0;
 						}
 						else{
-							$html .= '<tr class="font-weight-bold" style="background-color: #e5e5e5;">';
-							$html .= '<td colspan="2">'.$data->lineheading.'</td>';
-							$html .= '<td align="right">'.$ssubtotal.'</td>';
-							$html .= '</tr>';
+							echo '<tr class="font-weight-bold" style="background-color: #e5e5e5;">';
+							echo '<td colspan="2">'.$data->lineheading.'</td>';
+							echo '<td align="right">'.$ssubtotal.'</td>';
+							echo '</tr>';
 							$subtotal = 0;
 						}
 					}
@@ -197,7 +229,120 @@ class BalanceSheetController extends Controller
 				$dtotal = 0;
 			}
 		}
-
-		echo $html;
     }
+
+    /**
+     * Menampilkan data neraca multiperiod
+     * 
+     * @return \Illuminate\Http\Request
+     * @return \Illuminate\Http\Response
+     */
+    public function multiperiodData(Request $request)
+    {
+    	ini_set('max_execution_time', 300);
+		ini_set('memory_limit','1000M');
+
+		// Variabel
+		$grup = $request->query('grup');
+		$kantor = $request->query('kantor');
+		$bulan1 = $request->query('bulan1');
+		$bulan2 = $request->query('bulan2');
+		$tahun = $request->query('tahun');
+		$bulanString = $bulan1 <= 9 ? '0'.$bulan1 : $bulan1;
+		$tanggal = $tahun.'-'.$bulanString.'-01';
+		$accre = get_retained_earning($tanggal, $kantor, $grup)['akun']; // Akun laba ditahan
+		$rebalance = get_retained_earning($tanggal, $kantor, $grup)['balance']; // Balance laba ditahan
+		$delta = $bulan2 - $bulan1;
+		$adtotal = array();
+		$asubtotal = array(); // Total harta, kewajiban, modal
+		$apasiva = array();
+		$asdtotal = array();
+		$sysakun = "('3000','3001','5000')";
+
+		// Laba ditahan
+		$earning = [];
+		for($i=$bulan1; $i<=$bulan2; $i++){
+			$data = DB::select("SELECT COALESCE(SUM(g.transamount),0) AS earning FROM ac_glhist g INNER JOIN ac_akun a ON a.kodeakun=g.glaccount INNER JOIN ac_akuntipe t ON t.idtipe=a.idtipe WHERE t.grup IN(4,5) AND g.idkantor={$kantor} AND g.glperiod<={$i} AND g.glyear={$tahun}");
+			$earning[$i] = $data[0]->earning * -1;
+			$adtotal[$i] = 0;
+			$asubtotal[$i] = 0;
+			$apasiva[$i] = 0;
+			$asdtotal[$i] = 0;
+		}
+		
+		// Data neraca
+		$neraca = DB::select("SELECT a.*, (SELECT grup FROM ac_akuntipe WHERE idtipe=a.akuntipe) AS grup, (SELECT COUNT(kodeakun) FROM ac_akun WHERE idtipe=a.akuntipe) AS cnt FROM ac_fstmtlin a WHERE tipe=1 ORDER BY seq ASC");
+
+		// Display table
+		echo '<thead class="bg-warning">';
+		echo '<tr>';
+		echo '<th>Deskripsi</th>';
+		foreach(array_indo_month() as $key=>$data){
+			if($key+1 >= $bulan1 && $key+1 <= $bulan2){
+				echo '<th width="150">'.$data.'</th>';
+			}
+		}
+		echo '</tr>';
+		echo '</thead>';
+		echo '<tbody>';
+
+		// Loop neraca
+		foreach($neraca as $data){
+			// Reset pasiva
+			if($data->seq == 30){
+				for($i=$bulan1; $i<=$bulan2; $i++){
+					$apasiva[$i] = 0;
+				}
+			}
+
+			if($data->linetype == 'ListDetail'){
+				$sql = "SELECT kodeakun, namaakun ";
+				$j = 1;
+				for($i=$bulan1; $i<=$bulan2; $i++){
+					$iString = $i <= 9 ? '0'.$i : $i;
+					$sql .= ", (SELECT COALESCE(SUM(transamount),0) FROM ac_glhist WHERE glaccount=a.kodeakun AND idkantor={$kantor} AND transdate<'{$tahun}-{$iString}-01') AS b{$j}";
+					$j++;
+				}
+				$sql .= " FROM ac_akun a WHERE a.idtipe={$data->akuntipe} AND (a.kantorlist REGEXP('^\\\.{$kantor}$|\\\.{$kantor}\\\.|\\\.{$kantor}$') OR (kode IN ".$sysakun.")) AND a.groupid={$grup} ORDER BY a.kodeakun";
+				$rows = DB::select($sql);
+				
+				// Loop rows
+				if(count($rows)>0){
+					foreach($rows as $row){
+						$row = (array)$row;
+						echo '<tr>';
+						echo '<td style="text-indent: 1rem;">'.$row['namaakun'].'</td>';
+						$j = 1;
+						for($i=$bulan1; $i<=$bulan2; $i++){
+							$balance = $row['b'.$j];
+							if($data->grup > 1 && $balance != 0) $balance = $row['b'.$j] * -1;
+							if($row['kodeakun'] == $accre) $balance = $rebalance;
+							$adtotal[$i] += $balance; // Total detail
+							$asubtotal[$i] += $balance; // Total parent
+							$apasiva[$i] += $balance;
+							$sbalance = ($balance < 0 ? '('.number_format(abs($balance),0,',','.').')' : number_format($balance,0,',','.'));
+							$asdtotal[$i] = ($adtotal[$i] < 0 ? '('.number_format(abs($adtotal[$i]),0,',','.').')' : number_format($adtotal[$i],0,',','.'));
+							echo '<td style="text-align: right;">'.$sbalance.'</td>';
+							$j++;
+						}
+						echo '</tr>';
+					}
+				}
+			}
+			elseif($data->linetype == 'Detail'){
+				echo '<tr>';
+				echo '<td style="text-indent: 1rem";>'.$data->lineheading.'</td>';
+				for($i=$bulan1; $i<=$bulan2; $i++){
+					$searning = ($earning[$i] < 0 ? '('.number_format(abs($earning[$i]),0,',','.').')' : number_format($earning[$i],0,',','.'));
+					echo '<td style="text-align: right;">'.$searning.'</td>';
+					$apasiva[$i] = $apasiva[$i] + $earning[$i];	
+					$adtotal[$i] = $adtotal[$i] + $earning[$i];
+					$asubtotal[$i] = $asubtotal[$i] + $earning[$i];
+				}
+				echo "</tr>";
+			}
+		}
+
+		echo '</tbody>';
+	}
 }
